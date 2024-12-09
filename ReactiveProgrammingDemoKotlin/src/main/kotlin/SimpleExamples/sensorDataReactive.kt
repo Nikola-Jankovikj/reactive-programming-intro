6700fbf9b5527cf7b537d0f74a7013d8b50c2c29
srbinovskia46 <andrej.srbinovski@students.finki.ukim.mk>
2024-12-09 12:45:17 +0100

    Minor changes in code style

--- ReactiveProgrammingDemoKotlin/src/main/kotlin/SimpleExamples/sensorDataReactive.kt
+++ ReactiveProgrammingDemoKotlin/src/main/kotlin/SimpleExamples/sensorDataReactive.kt
@@ -2,29 +2,30 @@ package SimpleExamples
 
 // --- Sensor data stream reactive
 
-import reactor.core.publisher.Flux
-import reactor.core.publisher.Mono
-import java.time.Duration
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.kotlin.toObservable
+import java.util.concurrent.TimeUnit
 import kotlin.random.Random
 
 fun main() {
     val startTime = System.currentTimeMillis()
 
     // Simulate making asynchronous calls to 10 APIs (e.g., fetching sensor data from a remote service)
-    val sensorDataStream = Flux.range(1, 10)
+    val sensorDataStream = (1..10).toObservable()
         .flatMap { getSensorDataFromApiReactive(it) }
         .doOnNext { println(it) }
 
-    sensorDataStream.blockLast()
+    sensorDataStream.blockingLast()
 
     val endTime = System.currentTimeMillis()
     println("Reactive execution time: ${endTime - startTime} ms")
 }
 
-fun getSensorDataFromApiReactive(id: Int): Mono<String> {
+fun getSensorDataFromApiReactive(id: Int): Observable<String> {
     // Simulating an asynchronous call to an external API
-    return Mono.just("Sensor $id: Temperature ${Random.nextInt(20, 30)}°C")
-        .delayElement(Duration.ofMillis(Random.nextLong(100, 200)))  // Simulate network delay
+    return Observable.just("Sensor $id: Temperature ${Random.nextInt(20, 30)}°C")
+        .delay(Random.nextLong(100, 200), TimeUnit.MILLISECONDS)  // Simulate network delay
 }
+
 
 
